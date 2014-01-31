fcaefdc73a1942a188689f260b3ebc35ef0d907a
Anthony Roy <work@antroy.co.uk>
2014-01-31 20:21:43 +0000

    Init script checks out pelican_master into output

--- init.sh
+++ init.sh
@@ -4,3 +4,14 @@ mkdir content
 mkdir output
 
 echo "output" >> .gitignore
+
+if [ ! -d output/.git ]
+then
+    pushd output
+    git init
+    git remote add origin git@github.com:antroy/antroy.github.io.git
+    git fetch
+    git checkout -b pelican_master
+    git branch --set-upstream-to=origin/pelican_master pelican_master
+    popd
+fi
